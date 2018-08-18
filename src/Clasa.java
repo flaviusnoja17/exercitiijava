01a8bd526825e4486b70e5ca78015cffb89ec6ba
Flavius <flaviusnoja@outlook.com>
2018-08-18 11:46:46 +0300

    program input

---
+++ src/Clasa.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Clasa {
+public static void main (String[]arg){
+
+    //creaza scanner input
+    Scanner input = new Scanner(System.in);
+    //output the prompt
+    System.out.println("adauga text:");
+    //asteapta ca userul sa  introduca linia de text
+    String line = input.nextLine();
+    //arata ce a introdus
+    System.out.println("ai introdus" + line);
+
+}
+
+}
+
+
+
+
