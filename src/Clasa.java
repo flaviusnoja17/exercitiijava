5f73c16d3d46395be232e6064204083a2104e8de
Flavius <flaviusnoja@outlook.com>
2018-08-19 07:48:56 +0300

    exercitiu1

--- src/Clasa.java
+++ src/Clasa.java
@@ -1,35 +1,11 @@
-import java.util.Random;
-import java.util.Scanner;
 
 public class Clasa {
-    public static void main(String[] arg) {
+    public static void main (String[]arg){
 
-        int value = 7;
-
-        int[] values;
-
-        values = new int[3];
-        System.out.println(values [0]);
-
-        values[0] = 10;
-        values[1] = 20;
-        values[2] = 30;
-
-        System.out.println(values[0]);
-        System.out.println(values[1]);
-        System.out.println(values[2]);
-
-        for (int i= 0; i<values.length; i++);
-        System.out.println(values[i]);
-
-        }
- int[] numbers = {
-
-            5,6,7
- }
- for (int i=0; i<numbers.length; i++)
+        int a=7;
+        int b=10;
+        //scrie ceva aici
+        System.out.println("int a="+a+" b="+b);
     }
 
-
-
-
+}
