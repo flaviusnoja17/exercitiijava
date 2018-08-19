72c0031c49184adc50f2541046f9122fee3fb3f9
Flavius <flaviusnoja@outlook.com>
2018-08-19 08:00:45 +0300

    exercitiu1 primul while loop

--- src/Clasa.java
+++ src/Clasa.java
@@ -4,12 +4,25 @@ import java.util.Scanner;
 public class Clasa {
     public static void main (String[]arg){
 
-        for (int i=0; i < 5; i = i+1){
+        //creaza scanner input
+        Scanner scanner = new Scanner(System.in);
+        //output the prompt
+   /* System.out.println("adauga un numar");
+    int value = scanner.nextInt();
+    while (value != 5){
 
-            System.out.println("hello");
+        System.out.println("adauga numarul");
+        value = scanner.nextInt();
+    } */
+        int value = 0;
+        do {
+            System.out.println("adauga un nr");
+            value = scanner.nextInt();
+
 
         }
-
+        while ( value != 5);
+        System.out.println("ai 5");
     }
 
 }
