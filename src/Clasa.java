49c0cd102491104bf7385772c4b1f8f3ff0f27ef
Flavius <flaviusnoja@outlook.com>
2018-08-19 07:51:02 +0300

    exercitiu1 2 switch

--- src/Clasa.java
+++ src/Clasa.java
@@ -1,9 +1,12 @@
-
 public class Clasa {
     public static void main (String[]arg){
 
         int a=7;
         int b=10;
+        int c;
+        c=b;
+        b=a;
+        c=a;
         //scrie ceva aici
         System.out.println("int a="+a+" b="+b);
     }
