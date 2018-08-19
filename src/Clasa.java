e0d8dc644271f67d8ced753d9ba683b142dd71c7
Flavius <flaviusnoja@outlook.com>
2018-08-19 07:44:01 +0300

    program input

--- src/Clasa.java
+++ src/Clasa.java
@@ -2,20 +2,33 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Clasa {
-public static void main (String[]arg){
+    public static void main(String[] arg) {
 
-    //creaza scanner input
-    Scanner input = new Scanner(System.in);
-    //output the prompt
-    System.out.println("adauga text:");
-    //asteapta ca userul sa  introduca linia de text
-    String line = input.nextLine();
-    //arata ce a introdus
-    System.out.println("ai introdus" + line);
+        int value = 7;
 
-}
+        int[] values;
 
-}
+        values = new int[3];
+        System.out.println(values [0]);
+
+        values[0] = 10;
+        values[1] = 20;
+        values[2] = 30;
+
+        System.out.println(values[0]);
+        System.out.println(values[1]);
+        System.out.println(values[2]);
+
+        for (int i= 0; i<values.length; i++);
+        System.out.println(values[i]);
+
+        }
+ int[] numbers = {
+
+            5,6,7
+ }
+ for (int i=0; i<numbers.length; i++)
+    }
 
 
 
